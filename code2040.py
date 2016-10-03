eb5c80756b591d17b45830a3dcb50ed65935a6d5
Christopher Reyes <christophereyes.cr@gmail.com>
2016-10-03 17:14:54 -0500

    Working API call

--- code2040.py
+++ code2040.py
@@ -5,5 +5,14 @@ chris_api_token = 'b22eefe6ad7d9acfc8f1bcebf522d11a'
 github_address = 'https://github.com/christophereyes/code2040.git'
 payload = {'token':chris_api_token, 'github': github_address}
 url = 'http://challenge.code2040.org/api/register'
-r = requests.post(url, data=json.dumps(payload))
+
+print(payload)
+
+print(chris_api_token, github_address)
+
+r = requests.post(url, json=payload)
+
+print(r)
+print(r.text)
 r.json()
+
